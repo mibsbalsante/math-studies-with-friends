2ed7c5ede88baf942ba76e82f2d46398dc887bec
Manoj Fernando <mj.bitwave@gmail.com>
2017-10-31 06:28:34 +0530

    Add a math challenge #5

---
+++ prime_numbers/README.md
@@ -0,0 +1,7 @@
+# Challenge
+Find all prime numbers between 1 to 100
+
+---
+
+## Description:
+A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. The task is to find all the prime numbers between 1 to 100.
---
+++ prime_numbers/mjzone.js
@@ -0,0 +1,11 @@
+for (counter = 0; counter <= 100; counter++) {
+  var notPrime = false;
+  for (var i = 2; i <= counter; i++) {
+    if (counter % i === 0 && i !== counter) {
+      notPrime = true;
+    }
+  }
+  if (notPrime === false) {
+    console.log(counter);
+  }
+}
