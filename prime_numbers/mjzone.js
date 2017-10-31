a5a89c641cb5c82d5b6db5c19396b541beae8c24
Syyam Noor <syyamnoor@gmail.com>
2017-10-31 05:29:43 -0700

    Use 'let' instead of 'var'
    Please use 'let' instead of 'var'

--- prime_numbers/mjzone.js
+++ prime_numbers/mjzone.js
@@ -1,6 +1,6 @@
 for (counter = 0; counter <= 100; counter++) {
-  var notPrime = false;
-  for (var i = 2; i <= counter; i++) {
+  let notPrime = false;
+  for (let i = 2; i <= counter; i++) {
     if (counter % i === 0 && i !== counter) {
       notPrime = true;
     }
