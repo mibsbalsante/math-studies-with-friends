69084b382032fc3ec9ee817e81810638a97541a8
koreyGambill <korey.gam@gmail.com>
2017-10-30 23:03:36 -0600

    Created Check_Prime challenge and answer

---
+++ Check_Prime/koreygam.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public CheckPrime {
+
+  public static boolean isPrime(int n) {
+      if (n <= 1) {
+        return false;
+      }
+      for(int i=2; i<Math.sqrt(n); i++) {
+          if(n%i==0)
+            return false;
+      }
+      return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isPrime(31));
+  }
+  
+}
