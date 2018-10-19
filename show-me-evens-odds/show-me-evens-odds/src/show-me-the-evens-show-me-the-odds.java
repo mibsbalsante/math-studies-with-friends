69cb0d9ca278a6871fd3d5526bd4cc9b466e92e1
Kienan T Rose <ktr104@uxb4.wiu.edu>
2018-10-19 17:59:43 -0500

    wrote a small prgram to check for even and odd numbers and print numbers accordingly.

---
+++ show-me-evens-odds/show-me-evens-odds/src/show-me-the-evens-show-me-the-odds.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class show-me-the-evens-show-me-odds{
+   public class static void main(String[] args){
+   
+   Random rand = new Random();
+   rand = rand.nextInt();
+   
+   System.out.print(rand);
+
+
+   }
+}
